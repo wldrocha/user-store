9ef5f3b0181c4da072eb42991ac31f7c5504705e
Wladimir Rocha <wld.rocha@gmail.com>
2024-08-09 16:49:33 -0400

    feat: ✨ mongo connection

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from './config/envs';
+import { MongoDatabase } from './data';
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
 
@@ -8,7 +9,12 @@ import { Server } from './presentation/server';
 })();
 
 
-function main() {
+async function main() {
+
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  })
 
   const server = new Server({
     port: envs.PORT,
---
+++ src/data/mongo/mongo-database.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+interface ConnectionOptions {
+  mongoUrl: string
+  dbName: string
+}
+
+export class MongoDatabase {
+  static async connect(options: ConnectionOptions) {
+    const { mongoUrl, dbName } = options
+
+    try {
+      await mongoose.connect(mongoUrl, {
+        dbName
+      })
+
+      return true
+    } catch (error) {
+      console.error('Error connecting to MongoDB')
+      throw error
+    }
+  }
+}
