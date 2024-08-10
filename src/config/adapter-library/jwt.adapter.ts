2b18a9d2b6f0aafd32d3301d2d87c95d913dfeff
Wladimir Rocha <wld.rocha@gmail.com>
2024-08-10 12:18:57 -0400

    chore: 🚧 seed of jwt

--- src/config/adapter-library/jwt.adapter.ts
+++ src/config/adapter-library/jwt.adapter.ts
@@ -1,12 +1,20 @@
 import jwt from 'jsonwebtoken'
+import { envs } from '../envs'
 
+const JWT_SECRET = envs.JWT_SEED
+
+// Todo refactor hide dependency JWT_SECRET
 export class JWTAdapter {
   static async generateToken(payload: any, duration: string = '2h') {
     return new Promise((resolve) => {
-      jwt.sign(payload, 'SEED', { expiresIn: duration }, (err, token) => {
+      jwt.sign(payload, JWT_SECRET, { expiresIn: duration }, (err, token) => {
         if (err) resolve(null)
         resolve(token)
       })
     })
+  }
+  // Todo incomplete method
+  static validateToken(token: string) {
+    throw new Error('Method not implemented.')
   }
 }
