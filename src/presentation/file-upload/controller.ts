e4c0627f66ef804957667b2c55896fd4f2fefb73
Wladimir Rocha <wld.rocha@gmail.com>
2024-08-21 17:16:43 -0400

    feat: 🚧 upload images
    create controller, create route and implement controller on routes

---
+++ src/presentation/file-upload/controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+import { CustomError } from '../../domain'
+
+export class FileUploadController {
+  constructor() {}
+  private handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError) {
+      return res.status(error.statusCode).json({ error: error.message })
+    }
+    console.log('🚀 ~ AuthController ~ error:', error)
+    return res.status(500).json({ error: 'Internal server error' })
+  }
+
+  uploadFile = (req: Request, res: Response) => {
+    res.json({ message: 'File uploaded successfully' })
+  }
+  uploadMultipleFile = (req: Request, res: Response) => {
+    res.json({ message: 'Files uploaded successfully' })
+  }
+}
---
+++ src/presentation/file-upload/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import { FileUploadController } from './controller'
+
+export class FileUploadRoutes {
+  static get routes(): Router {
+    const router = Router()
+
+    const controller = new FileUploadController()
+
+    router.post('/single/:type', controller.uploadFile)
+    router.post('/multiple/:type', controller.uploadMultipleFile)
+
+    return router
+  }
+}
