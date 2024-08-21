3f35f48e8135716fbad3de81050f6d1ec3c1b298
Wladimir Rocha <wld.rocha@gmail.com>
2024-08-21 16:37:01 -0400

    feat: ✨ create seed

--- src/data/seed/seed.ts
+++ src/data/seed/seed.ts
@@ -1,6 +1,5 @@
+import { CategoryModel, MongoDatabase, ProductModel, UserModel, seedData } from '..'
 import { envs } from '../../config'
-import { MongoDatabase } from '../mongo/mongo-database'
-
 ;(async () => {
   await MongoDatabase.connect({
     dbName: envs.MONGO_DB_NAME,
@@ -10,7 +9,38 @@ import { MongoDatabase } from '../mongo/mongo-database'
   await main()
 
   MongoDatabase.disconnect()
-
 })()
 
-async function main() {}
+async function main() {
+  /* 
+    Todo: delete all data from the database
+
+    Todo: create users
+
+    Todo: create categories
+
+    Todo: create products
+  
+  */
+
+  const randomBetween0andX = (x: number) => Math.floor(Math.random() * x)
+
+  await Promise.all([UserModel.deleteMany(), CategoryModel.deleteMany(), ProductModel.deleteMany()])
+
+  const users = await UserModel.insertMany(seedData.users)
+
+  const categories = await CategoryModel.insertMany(
+    seedData.categories.map((category) => ({
+      ...category,
+      user: users[randomBetween0andX(users.length)]._id
+    }))
+  )
+
+  const products = await ProductModel.insertMany(
+    seedData.products.map((product) => ({
+      ...product,
+      user: users[randomBetween0andX(users.length -1)]._id,
+      category: categories[randomBetween0andX(categories.length -1)]._id
+    }))
+  )
+}
