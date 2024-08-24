52908e3ed6edc2057d5906446dd014aabe79fd9c
Wladimir Rocha <wld.rocha@gmail.com>
2024-08-24 11:51:41 -0400

    chore: 🚧 sub directories

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -9,11 +9,18 @@ export class FileUploadController {
     if (error instanceof CustomError) {
       return res.status(error.statusCode).json({ error: error.message })
     }
-    
+
     return res.status(500).json({ error: 'Internal server error' })
   }
 
   uploadFile = (req: Request, res: Response) => {
+    const type = req.params.type
+    const validTypes = ['users', 'products', 'categories']
+
+    if (!validTypes.includes(type)) {
+      return res.status(400).json({ error: `Invalid type: ${type}, valid ones ${validTypes}` })
+    }
+
     const files = req.files
 
     if (!files || Object.keys(files).length === 0) {
@@ -22,7 +29,7 @@ export class FileUploadController {
 
     const file = req?.files?.file as UploadedFile
     this.fileUploadFileService
-      .uploadSingle(file)
+      .uploadSingle(file, `uploads/${type}`)
       .then((uploaded) => res.json(uploaded))
       .catch((error) => this.handleError(error, res))
   }
